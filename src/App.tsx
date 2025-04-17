5f3c940a05180d45ac6cf8689bb1acf6ed6458a1
Vlad Mantur <Colardra1@gmail.com>
2025-04-17 17:27:41 +0300

    added initial set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <>
-      <p className="text-center text-5xl text-red-500 font-black ">HELLO, WORLD!</p>
-    </>
-  );
+  return <div className=""></div>;
 }
 
 export default App;
