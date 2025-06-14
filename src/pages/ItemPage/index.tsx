9c7bc2c6087ff7518656b63b1feb65c9043f792d
Vlad Mantur <Colardra1@gmail.com>
2025-06-14 23:30:48 +0300

    Optimized ItemPage component rerendering

--- src/pages/ItemPage/index.tsx
+++ src/pages/ItemPage/index.tsx
@@ -7,6 +7,8 @@ import useItemLinkPath from "./useItemLinkPath";
 import ItemIamges from "../../sections/ItemIamgesBlock";
 import ItemPageInstruments from "../../sections/ItemPageInstruments";
 import useNotFoundRedirect from "../../hooks/useNotFoundRedirect";
+import { memo } from "react";
+import { Product } from "../../types/api.types";
 
 const tabMenuItems = [
   { title: "Description", callBack: () => {} },
@@ -21,16 +23,14 @@ let ItemPage: React.FC = () => {
   );
 
   useNotFoundRedirect(!product && !isLoading);
-  const linkPath = useItemLinkPath(product);
 
   return (
     <div>
       <h1 className="sr-only">{product?.title || ""}</h1>
       <Container>
-        <LinkWay pagePath={linkPath} className="my-5 md:mt-6 md:mb-9"></LinkWay>
         <section className="gap-10 xl:flex">
           <h2 className="sr-only">General info</h2>
-
+          <ProductLinkWay product={product} />
           <ItemIamges
             className="h-image-block mx-auto w-full shrink-0 xl:mx-0 xl:max-w-152.5"
             images={product?.images || []}
@@ -53,3 +53,10 @@ let ItemPage: React.FC = () => {
 };
 
 export default ItemPage;
+
+const ProductLinkWay: React.FC<{ product: Product | null }> = ({ product }) => {
+  const linkPath = useItemLinkPath(product);
+  return (
+    <LinkWay pagePath={linkPath} className="my-5 md:mt-6 md:mb-9"></LinkWay>
+  );
+};
